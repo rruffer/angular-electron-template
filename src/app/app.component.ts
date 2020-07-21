31dd08aa88a6b4687d48f3a4d93972fac019b0f8
unknown <rodolforuffer@yahoo.com.br>
2020-07-21 11:33:18 -0300

    arquivos iniciais

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+/**
+ * https://www.intertech.com/electron-tutorial-getting-started-with-electron-and-angular-cli/
+ * https://github.com/dalenguyen/todo-app-electron-angular
+ * https://github.com/SamGraber/angular-electron-forge
+ * https://www.electronjs.org/docs
+ */
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-electron';
+}
---
+++ src/electronMain.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+let win;
+
+function isDev() {
+  return process.mainModule.filename.indexOf('app.asar') === 1;
+}
+
+function createWindows() {
+  win = new BrowserWindow({
+    width: 600,
+    height: 600,
+    backgroundColor: '#ffffff',
+    icon: path.join(__dirname, '../dist/assets/logo.png')
+  });
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, '../dist/index.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+
+  win.webContents.openDevTools();
+
+  win.on('close', function () {
+    win = null;
+  })
+
+}
+
+
+
+app.on('ready', createWindows);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindows();
+  }
+})
+
